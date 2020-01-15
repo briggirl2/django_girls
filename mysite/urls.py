35404131bdb7dbfeea9d6c830d664e65dfc0eb8e
briggirl2 <briggirl2@gmail.com>
2020-01-16 00:02:06 +0900

    safe website complete

--- mysite/urls.py
+++ mysite/urls.py
@@ -13,17 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+# from django.conf.urls import url, include
 from django.contrib import admin
-# from django.urls import path, include
+from django.urls import path, include
 
-from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('', include('blog.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'', include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+
+    path('accounts/login/', auth_views.LoginView.as_view(),  name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout', kwargs={'next_page': '/'})
 ]
